1b9ef4d8140166eb6271d33c18e5a88593f0e58b
pashupati-singh <119448686+pashupati-singh@users.noreply.github.com>
2024-03-10 04:12:11 +0530

    app

--- crypto-app/src/Components/Markets.jsx
+++ crypto-app/src/Components/Markets.jsx
@@ -4,7 +4,7 @@ import Coin from "./Coin";
 import {Skeleton} from "./Skeleton";
 
 export const Markets = () => {
-    const { response, loading } = useAxios('coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    const { response, loading } = useAxios('coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en');
 
   if(loading) {
     return (
